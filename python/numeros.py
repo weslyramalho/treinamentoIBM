50c9b7ce822a70a9ee64a2e9091e6a9205d33e0a
weslyramalho <weslyramalho@yahoo.com.br>
2023-09-05 14:36:17 -0300

    numeros

---
+++ python/numeros.py
@@ -0,0 +1,7 @@
+import math
+
+print(math.pi *4)
+print(math.pi **2)
+print(math.sqrt(4))
+
+
--- python/primeiro.py
+++ python/primeiro.py
@@ -1,7 +1,8 @@
-
+print("hello word!")
 x = "O valor de (a+b)*c é"
 a, b, c = 4, 3, 2
 d = (a + b) * c
 imprimir = True
 if imprimir:
  print(x, d)
+help()
