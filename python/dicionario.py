b3c3c54f993e4786740b5b8883aea96937d6af93
weslyramalho <weslyramalho@yahoo.com.br>
2023-09-06 17:44:39 -0300

    dicionario

---
+++ python/dicionario.py
@@ -0,0 +1,16 @@
+dic = {
+    "Nombre":"Santiago",
+    "Apellido":"Hernandez",
+    "Pais":"España",
+    "Ciudad":"Madrid"
+}
+print(dic)
+
+dic2 = dict(
+Nombre="Santiago",
+Apellido="Hernandez",
+Pais="España",
+Ciudad="Madrid"
+)
+
+print(dic2)
