097b745a60315b19bf8efc33f9a8138d983e05e1
weslyramalho <weslyramalho@yahoo.com.br>
2023-09-12 19:22:38 -0300

    temporario

--- python/empregado/empregado.py
+++ python/empregado/empregado.py
@@ -21,5 +21,70 @@ class Empregado:
     
     def get_fone(self):
         return self._fone
+
+class Efetivo(Empregado):
+    def __init__(self, nome, salario, fone, registro, salarioBase, duracao_contrato):
+        super().__init__(nome, salario, fone)
+        
+        self._registro = registro
+        self._salarioBase = salarioBase
+        self._duracao_contrato = duracao_contrato
+
+    def set_registro(self, registro):
+        self._registro = registro
+
+    def get_registro(self):
+        return self._registro
+    
+    def set_salarioBase(self, salarioBase):
+        self._salarioBase = salarioBase
+
+    def get_slarioBase(self):
+        return self._salarioBase
+    
+    def set_duracaoContrato(self, duracao_contrato):
+        self._duracao_contrato = duracao_contrato
+    
+    def get_duracaoContrato(self):
+        return self._duracao_contrato
+    
+    def calcularSalarioTotal(self):
+        return self._salarioBase + (self._salarioBase * 0.02)
+    
+    class Indeterminado(Empregado):
+        def __init__(self, nome, salario, fone, registro, salarioBase, categoria):
+            super().__init__(nome, salario, fone)
+            self._registro = registro
+            self._salarioBase = salarioBase
+            self._categoria = categoria
+
+        def set_registro(self, registro):
+          self._registro = registro
+
+        def get_registro(self):
+             return self._registro
+
+        def set_salarioBase(self, salarioBase):
+            self._salarioBase = salarioBase
+
+        def get_slarioBase(self):
+             return self._salarioBase
+        
+        def set_categoria(self, categoria):
+            self._categoria = categoria
+
+        def get_categoria(self):
+            return self._categoria
+        
+        def calcularSalarioTotal(self):
+            if self._categoria == 1:
+                return self._salarioBase + (self._salarioBase * 0.03)
+            elif self._categoria == 2:
+                return self._salarioBase + (self._salarioBase * 0.05)
+            elif self._categoria == 3:
+                return self._salarioBase + (self._salarioBase * 0.08)
+            else: 
+                return self._salarioBase
+        
     
     
