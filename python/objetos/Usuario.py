9fda342cc6636579a962759b9e5bd17be2dc45a7
weslyramalho <weslyramalho@yahoo.com.br>
2023-09-12 14:58:19 -0300

    construtor

--- python/objetos/Usuario.py
+++ python/objetos/Usuario.py
@@ -6,6 +6,14 @@ class Usuario():
     email = "angel@angel.com"
     fone = 0000000
 
+    def __init__(self):
+        self.nome
+        self.idade
+        self.login
+        self.senha
+        self.email
+        self.fone
+
     def imprimeUsuarios(self):
         print(f'Dados dos usuarios: \n'
               f'Nome: {self.nome}\n'
--- python/objetos/test.py
+++ python/objetos/test.py
@@ -1,3 +1,6 @@
 
-administrador = Usuario()
-administrador.imprimeUsuario()
+from objetos.Usuario import Usuario
+
+
+admin = Usuario()
+admin.imprimeUsuarios()
