10bb93bcd41f3380c8cdb474c0ec539228644d96
weslyramalho <weslyramalho@yahoo.com.br>
2023-09-15 17:10:40 -0300

    dataframae

---
+++ python/dataframes.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+dicionario = {'Autores' : ['Rick Riordan', 'J.R.R Tolkien', 'Rabelo', 'Machado de Assis'],
+              'Titulo': ['O Ladrão de Raios', 'A sociedade do Anel', 'Mar de Monstros', 'Memorias'],
+              'Preço': [41.2, 35.7, 39.5, 40.5]}
+df= pd.DataFrame(dicionario)
+df
